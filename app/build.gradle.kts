6e3f4e36af1b17a5b6a072005b03818e5ae0989c
RaymondThiago13 <tnraymond2003@gmail.com>
2026-05-15 17:19:41 +0200

    Creation

---
+++ app/build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.cryptocurrency_miniproject"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.cryptocurrency_miniproject"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    //Navigation
+    //implementation("androidx.navigation:navigation-compose:2.9.8")
+    implementation(libs.androidx.navigation.compose)
+
+    //ViewModel
+    //implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    //Coil
+    //implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
+    //implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation(libs.coil.network.okhttp)
+    implementation(libs.coil.compose)
+
+    //RetroFit for API Calls
+    //implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    //implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    //VideoPlayer
+    //implementation("androidx.media3:media3-exoplayer:1.10.1")
+    //implementation("androidx.media3:media3-ui:1.10.1")
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
+
+
+}
---
+++ app/src/main/java/com/example/cryptocurrency_miniproject/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.cryptocurrency_miniproject.utils
+
+object Constants {
+
+}
